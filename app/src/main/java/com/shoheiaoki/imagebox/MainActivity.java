225422d6bf019455769af7899ceac29b208f709f
Shohei Aoki <shoaok@gmail.com>
2016-07-02 13:17:13 +0900

    implement listview

--- app/src/main/java/com/shoheiaoki/imagebox/MainActivity.java
+++ app/src/main/java/com/shoheiaoki/imagebox/MainActivity.java
@@ -1,13 +1,31 @@
 package com.shoheiaoki.imagebox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView algoListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        algoListView = (ListView) findViewById(R.id.algoListView);
+        String[] algoList = getResources().getStringArray(R.array.image_processing_algorithms);
+        ArrayList<String> algoNames = new ArrayList<>();
+        for(String a : algoList){
+            int algoId = this.getResources().getIdentifier(a,"string",this.getPackageName());
+            String algoName = getResources().getString(algoId);
+            algoNames.add(algoName);
+        }
+
+        ArrayAdapter<String> algoAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,algoNames);
+        algoListView.setAdapter(algoAdapter);
     }
 }
